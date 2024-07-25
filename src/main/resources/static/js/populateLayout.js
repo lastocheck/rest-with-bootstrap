8b23675513e0e3f3462460a3ddf6dbf693db23b2
lastocheck <lastocheck@gmail.com>
2024-07-25 14:19:13 +0300

    Add sidebar population from JS

--- src/main/resources/static/js/populateLayout.js
+++ src/main/resources/static/js/populateLayout.js
@@ -1,4 +1,21 @@
 $.get('http://localhost:8080/api/v1/user', (user) => {
     $('#headerUsername').text(user.username);
     $('#headerRoles').text(user.roles);
+
+    const path = $(location).attr("pathname");
+
+    const userLink = $('<a href="/user" class="nav-link">User</a>');
+    const adminLink = $('<a href="/admin" class="nav-link">Admin</a>');
+
+    for (const link of [userLink, adminLink]) {
+        if (path.includes(link.attr('href'))) {
+            link.addClass('active');
+        }
+    }
+
+    $('#sidebarNav').append(userLink);
+
+    if (user.roles.includes('ADMIN')) {
+        $('#sidebarNav').append(adminLink);
+    }
 });
